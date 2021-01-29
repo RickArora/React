3ef1736d40e885b0df5c759c00f8c8f0abdaee66
Ricky Arora <your_email_here>
2021-01-29 12:13:29 -0500

    fixed entry?

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: "./frontend/my_app.jsx",
+    entry: "entry.jsx",
     devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
