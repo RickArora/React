36f888a0ea0a46d130e2f8d8ae45538c6951574d
Ricky Arora <your_email_here>
2021-02-02 12:47:53 -0500

    bundlejs issues

--- app.jsx
+++ app.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './calculator';
+import Congrats from './calculator'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Calculator/>, root);
+  ReactDOM.render(<Congrats/>, root);
 });
